cb85269f393833bcd1483cc1112d25d04497309a
Amanda Ocampo <aocampo@cuc.edu.co>
2021-09-12 13:40:20 -0500

    ejercicio 2

--- taller_condicionales.py
+++ taller_condicionales.py
@@ -22,3 +22,32 @@ else:
 total = sub_total-desc
 print('El total a pagar es: ', total)
 print('El descuento realizado fue: ', desc)
+
+# En un supermercado se hace una promoción, mediante la cual el cliente
+# obtiene un descuento dependiendo de un número que se escoge al azar.
+# Si el número escogido es menor que 74 el descuento es del 15% sobre el
+# total de la compra, si es mayor o igual a 74 el
+# descuento es del 20%. Obtener cuanto dinero se le descuenta.
+
+compra = int(input('Digite el valor de la compra'))
+num = int(input('Digite un número: '))
+desc = 0
+if(num < 74):
+    desc = compra*0.15
+    print('Usted recibio un descuento del 15%')
+else:
+    desc = compra*0.20
+    print('Usted recibio un descuento del 20%')
+total = compra - desc
+print('El descuento realizado fue', desc)
+print('El total a pagar es: ', total)
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
